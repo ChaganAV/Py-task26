49d8ed0ed1c236880415f43e8a669180ba69ac96
Alexandr <jwchaganav@gmail.com>
2023-01-15 22:24:29 +0400

    добавил try

--- py-task26.py
+++ py-task26.py
@@ -1,11 +1,17 @@
-A = int(input("Введите число А: "))
-B = int(input("Введите число B: "))
-
 def power(A,B):
     if B>0:
         return A*power(A,B-1)
     else:
-        return 1
+        if B<0:
+            print("Мы так не договаривались, введите положительную степень")
+            return 0
+        else:
+            return 1
 
-res = power(A,B)
-print(res)
+try:
+    A = int(input("Введите число А: "))
+    B = int(input("Введите число B: "))
+    res = power(A,B)
+    print(res)
+except:
+    print("Что-то пошло не так")
